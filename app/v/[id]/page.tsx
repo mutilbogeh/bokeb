4d320c5e110899fc11ad245f7c102597425856db
mutilbogeh <mutilbogeh@gmail.com>
2025-02-17 09:32:52 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -14,8 +14,6 @@ import { SITENAME } from "@/lib/constants";
 import Script from "next/script";
 
 import { Button } from "@/components/ui/button";
-import CopyButton from "@/components/copy-button";
-import LikeButton from "@/components/like-button";
 import Link from "next/link";
 import MessageBox from "@/components/message-box";
 import React from "react";
@@ -40,8 +38,8 @@ export async function generateMetadata(
     }
 
     const file = data.result[0];
-    const title = `${file.title}`;
-    const description = `${file.title} di ${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma`;
+    const title = `${file.file_title}`;
+    const description = `${file.file_title} di ${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma`;
     const image = file.thumbnail;
     const previousOgImages = (await parent).openGraph?.images || [];
     const previousTwImages = (await parent).twitter?.images || [];
@@ -81,72 +79,19 @@ export default async function Video({ params }: PageProps) {
         );
     }
 
-    const file = data.result[0];
-        const jsonLd2 = {
-        '@context': 'https://schema.org',
-        '@type': 'Article',
-        headline: `${file.title}`,
-        image: file.thumbnail,
-        description: `${file.title} di ${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma`,
-        url: `https://bokeb.pages.dev/v/${file.filecode}`,
-        datePublished: new Date(
-            file.uploaded + ".000Z"
-        ).toISOString(),
-        publisher: {
-            '@type': 'Organization',
-            name: `${SITENAME}`,
-            logo: 'https://bokeb.pages.dev/favicon.ico'},
-            author: {
-                '@type': 'Person',
-                name: 'admin',
-                url: 'https://bokeb.pages.dev'
-              },
-        interactionStatistic: {
-            '@type': `InteractionCounter`,
-                userInteractionCount: `${file.views}`,
-            interactionType: {
-                '@type': `ReadAction`,
-                target: `https://bokeb.pages.dev/v/${file.filecode}`
-            }  
-        }
-        }
-        const jsonLd3 = {
-            '@context': 'https://schema.org', 
-            '@type': 'Book', 
-            'name': `${file.title}`, 
-            'aggregateRating': {
-            '@type': 'AggregateRating',	
-                'ratingValue': '5',	
-                'ratingCount': `${file.views}`,	
-                'bestRating': '5',	
-                'worstRating': '1' }
-        }
-    return (
-        <div className="grid col-span-full gap-4 md:gap-4 md:mx-10">
-        <section>
-        {/* Add JSON-LD to your page */}
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd3) }}
-        />
-        {/* ... */}
-        </section>
+
             <iframe
                 className="w-full h-[30vh] md:h-[55vh] lg:h-[70vh]"
                 src={`https://filemoon.to/e/${file.filecode}`}
                 scrolling="no"
-                title={file.title}
+                title={file.file_title}
                 frameBorder={0}
                 allowFullScreen={true}
             ></iframe>
             <Card className="mx-2 mb-8">
                 <CardHeader>
                     <CardTitle className="text-xl md:text-3xl font-bold">
-                        {file.title}
+                        {file.file_title}
                     </CardTitle>
                 </CardHeader>
             </Card>
@@ -155,7 +100,7 @@ export default async function Video({ params }: PageProps) {
                 Related Video {file.title}
             </h2>
 <Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2w2e4z2e4&u=http%3A%2F%2Fwww.juicyads.rocks"/>
-            <SearchCardList query={file.title.split(" ")[2]} />
+            <SearchCardList query={file.file_title.split(" ")[2]} />
 <p>Video bokep indo terbaru viral korea china tante live paksa ngentot abg cewek pelajar pijat Hijab Abg Colmek Film Tante Hot Twitter Asia Download Live stw situs indonesia nonton link sd crot playbokep simontok bokepin montok baru perawan anak kecil telegram selingkuh ojol cantik gay vidio lokal artis pelajar Video Bokep indo jepang indonesia barat viral bokep video terbaru bocil bokepindo korea jilbab smp japan ama sub sma jav cina japanese anime hijab abg colmek film tante twitter asia china rusia no sensor hot arab india download live stw thailand situs hd nonton link baru mom montok telegram perawan sd anak kecil crot bokepindoh selingkuh cantik ojol lokal vidio gay asian anime amerika hentai artis streaming pelajar janda Video Bokep indo jepang indonesia barat viral bokep video terbaru bocil bokepindo korea jilbab smp japan ama sub sma jav cina japanese anime hijab abg colmek film tante twitter asia china rusia no sensor hot arab india download live stw thailand situs hd nonton link baru mom montok telegram perawan sd anak kecil crot bokepindoh selingkuh cantik ojol lokal vidio gay asian anime amerika hentai artis streaming pelajar janda hub sotwe olmek avtube pijat pure gudang pemerkosaan rumah tobrut inggris ngintip vcs binor yandex update remaja {file.title} wiki raja bokeptube full porno bokepmama simontok bokepind playbokep indobokep xpanasonline indoh</p>
         </div>
     );
