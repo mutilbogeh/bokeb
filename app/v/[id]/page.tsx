059bacf16735e3b0c21110601a462c4edd8157bc
mutilbogeh <mutilbogeh@gmail.com>
2025-02-17 14:07:19 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -69,9 +69,9 @@ export default async function Video({ params }: PageProps) {
 <meta itemProp="author" content="admin" />
 <meta itemProp="name" content={`${file.file_title}`} />
 <meta itemProp="description" content={`${file.file_title} di ${SITENAME} Video Bokep Indo Viral Terbaru Xpanas Bocil Ngentot Jilbab Smp Mama Sma`} />
-<meta itemProp="duration" content={`${new duration(
+<meta itemProp="duration" content={`${new Date(
             file.file_length
-        ).toISOString()}`} />
+        ).toTimeString()}`} />
 <meta itemProp="thumbnailUrl" content={`${file.player_img}`} />
 <meta itemProp="embedURL" content={`https://filemoon.to/e/${file.file_code}`} />
 <meta itemProp="uploadDate" content={`${new Date(
