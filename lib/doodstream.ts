36c842da3a7d9368aafa2cf9fe10379097fdfae4
mutilbogeh <mutilbogeh@gmail.com>
2025-02-17 12:36:55 +0700

    Update doodstream.ts

--- lib/doodstream.ts
+++ lib/doodstream.ts
@@ -82,7 +82,7 @@ class Doodstream {
     async search({ query }: { query: string }) {
         const data = await this.fetch(
             "/file/list",
-            { search_term: query },
+            { search_term: title },
             20
         );
         return data;
