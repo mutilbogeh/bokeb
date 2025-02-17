39e54641db27c17cbac65dcc8be2b99297b31d66
mutilbogeh <mutilbogeh@gmail.com>
2025-02-17 10:30:22 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -28,28 +28,5 @@ export default async function Video({ params }: PageProps) {
         );
     }
         const file = data.result[0];
-    return (console.log();
-<div className="grid col-span-full gap-4 md:gap-4 md:mx-10">
-            <iframe
-                className="w-full h-[30vh] md:h-[55vh] lg:h-[70vh]"
-                src={`https://filemoon.to/e/${file.file_code}`}
-                scrolling="no"
-                title={file.file_title}
-                frameBorder={0}
-                allowFullScreen={true}
-            ></iframe>
-            <Card className="mx-2 mb-8">
-                <CardHeader>
-                    <CardTitle className="text-xl md:text-3xl font-bold">
-                        {file.file_title}
-                    </CardTitle>
-                </CardHeader>
-            </Card>
-            <h2 className="text-2xl font-bold text-center my-4">
-                Related Video {file.file_title}
-            </h2>
-<Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2w2e4z2e4&u=http%3A%2F%2Fwww.juicyads.rocks"/>
-            <SearchCardList query={file.file_title.split(" ")[2]} />
-        </div>
-        );
+    return console.log();
 }
